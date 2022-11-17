879060b2a7f5e53fdc7d3a2fc35eb6882d842718
iconicsudip <das366966@gmail.com>
2022-11-18 01:19:26 +0530

    first commit 13

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({
-    origin:["http://localhost:3000","https://phtogall.netlify.app"],
     methods:"GET,POST,PUT,DELETE",
     credentials:true
 }))
